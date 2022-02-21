6bb79cdadc249c99043b4e464334380ac3b77678
tomyyn <tomasnaiouf@gmail.com>
2022-02-21 14:07:40 -0300

    Add listing function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let myLibrary=[];
+const bookList=document.querySelector(".bookList")
+
 
 function Book(title, author,pages,read){
     this.title=title;
@@ -6,7 +8,8 @@ function Book(title, author,pages,read){
     this.pages=pages;
     this.read=read;
     this.info = ()=>{
-        return `${title} by ${author}. ${pages} pages, `+read?"read":"not read yet";
+        let information=this.read?"read":"not read"
+        return `${this.title} by ${this.author}, ${this.pages} pages, ` + information + ".";
     }
 }
 
@@ -14,3 +17,20 @@ function addBookToLibrary(title, author,pages,read){
     
     myLibrary.push(new Book(title,author,pages, read));
 }
+
+function showBook(book){
+    const cont=document.createElement("div");
+    cont.textContent=book.info();
+    console.log(book.info())
+    bookList.appendChild(cont)
+}
+
+function showBooks(){
+    myLibrary.forEach(book=>
+        {showBook(book);}
+    )
+}
+
+
+addBookToLibrary("SampleTextOfASample", "Sigman", 600, true)
+showBooks();
